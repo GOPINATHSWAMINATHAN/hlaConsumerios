6b660e4031d3647678ceeef89275220fa74cc4ab
GOPINATH SWAMINATHAN <GOPINATHSWAMINADANE@GMAIL.COM>
2018-01-19 16:48:41 +0300

    Update AppDelegate.swift

--- NewPj1/AppDelegate.swift
+++ NewPj1/AppDelegate.swift
@@ -26,8 +26,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GMSServices.provideAPIKey("AIzaSyCaFeTKZYfPgm_YVhxuAmG0uGJEEmlqC7w")
-        GMSPlacesClient.provideAPIKey("AIzaSyCaFeTKZYfPgm_YVhxuAmG0uGJEEmlqC7w")
+        GMSServices.provideAPIKey("***************************************")
+        GMSPlacesClient.provideAPIKey("***********************************")
         FirebaseApp.configure()
         
         
